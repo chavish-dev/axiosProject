6751400eee0cfa14a88c593f1c91fe502ae3f14c
חוי שפיאצקי <207857814@MBYdomain.local>
2023-12-29 10:36:25 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MyComp from './Components/MyComp';
+
+function App() {
+  return (
+    <div className="App">
+     <MyComp/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/MyComp.jsx
@@ -0,0 +1,38 @@
+import react, { useEffect } from 'react'
+import axios from 'axios'
+
+const MyComp=()=>{
+    
+
+
+const fetchData=async()=>{
+
+    try{
+        debugger
+        const res =await fetch('https://jsonplaceholder.typicode.com/posts')
+        const data =await res.json()
+        console.log(data);
+    }catch(e){
+console.error(e)
+    }
+}
+
+const fetchAxios=async()=>{
+    debugger
+const res = await axios.get('https://jsonplaceholder.typicode.com/posts')
+console.log(res);
+}
+
+
+useEffect(()=>{
+    // fetchData()
+    fetchAxios()
+},[])
+    return(
+        <>
+
+        </>
+    )
+}
+
+export default MyComp
